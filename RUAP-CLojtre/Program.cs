b4f0536d600ef0baf67eeb9c47223ee0d7033b2f
lukamajstorovic <lmajstorovic@etfos.hr>
2023-10-31 17:25:32 +0100

    Add C# project

---
+++ RUAP-CLojtre/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RUAP_CLojtre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Luke Majstorovica");
+        }
+    }
+}
