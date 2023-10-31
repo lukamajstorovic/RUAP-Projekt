40e26b4ef5789b3cf4cbe5d95ad1bd53b913a046
lukamajstorovic <lmajstorovic@etfos.hr>
2023-10-31 17:32:10 +0100

    Add previous commit id in message

--- RUAP-CLojtre/Program.cs
+++ RUAP-CLojtre/Program.cs
@@ -6,7 +6,7 @@ namespace RUAP_CLojtre
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Pozdrav iz aplikacije od Luke Majstorovica");
+            Console.WriteLine("Pozdrav iz aplikacije od Luke Majstorovica b4f0536d600ef0baf67eeb9c47223ee0d7033b2f");
         }
     }
 }
