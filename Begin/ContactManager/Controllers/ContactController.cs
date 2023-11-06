7d03155325a170758ac218229fef6b01958639ea
lukamajstorovic <lmajstorovic@etfos.hr>
2023-11-06 19:23:26 +0100

    Exercise 3 Task 3

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -3,6 +3,7 @@ using ContactManager.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -21,6 +22,14 @@ namespace ContactManager.Controllers
         public Contact[] Get()
         {
             return contactRepository.GetAllContacts();
+        }
+        public HttpResponseMessage Post(Contact contact)
+        {
+            this.contactRepository.SaveContact(contact);
+
+            var response = Request.CreateResponse<Contact>(System.Net.HttpStatusCode.Created, contact);
+
+            return response;
         }
     }
 }
